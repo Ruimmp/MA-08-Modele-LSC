853147717888eaef0aba3865c5a6b99063c6a33b
MONTEIRO-PEREIRA Rui-Miguel <Rui-Miguel.MONTEIRO-PEREIRA@cpnv.ch>
2022-02-07 09:21:43 +0100

    Evaluation Commit

---
+++ Rendu/SelectDatabase LSC.sql
@@ -0,0 +1,50 @@
+USE LosSantosCustum;
+
+GO
+SELECT *
+FROM Brands
+ORDER BY Name ASC;
+
+GO
+SELECT *
+FROM Cars;
+
+GO
+SELECT *
+FROM Cars_has_Models;
+
+GO
+SELECT *
+FROM Cars_has_Sellers;
+
+GO
+SELECT *
+FROM Categories;
+
+GO
+SELECT *
+FROM Categories_has_Cars;
+
+GO
+SELECT *
+FROM Chassis;
+
+GO
+SELECT *
+FROM Customers;
+
+GO
+SELECT *
+FROM Locations;
+
+GO
+SELECT *
+FROM Models;
+
+GO
+SELECT *
+FROM Promotions;
+
+GO
+SELECT *
+FROM Sellers;
